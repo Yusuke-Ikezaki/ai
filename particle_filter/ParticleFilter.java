78b04e201ec9b0baf0aad7e6af78f8d8e707fae4
Yusuke Ikezaki <iappley08@gmail.com>
2017-07-22 19:23:18 +0900

    edit ParticleFilter.java and Test.java

--- particle_filter/ParticleFilter.java
+++ particle_filter/ParticleFilter.java
@@ -11,57 +11,66 @@ public class ParticleFilter{
   private int state_n;
   /* 粒子の数 */
   private int particle_n;
+  /* 観測成功率 */
+  private double observe_p;
+  /* 行動成功率 */
+  private double action_p;
   /* 粒子の分布 */
   private double[] particles;
   /* 観測確率 */
-  private double[][] observe_p;
+  private double[][] observes;
 
   /* コンストラクタ */
-  public ParticleFilter(int state_n, int particle_n){
+  public ParticleFilter(int state_n, int particle_n, double observe_p, double action_p){
     this.state_n = state_n;
     this.particle_n = particle_n;
+    this.observe_p = observe_p;
+    this.action_p = action_p;
     particles = new double[state_n];
-    observe_p = new double[state_n][state_n];
+    observes = new double[state_n][state_n];
+    init();
   }
 
   /* 初期化 */
   public void init(){
-    double num = particle_n / state_n;
-    for(int i = 0; i < state_n; i++)
-      particles[i] = num;
     for(int i = 0; i < state_n; i++){
       if(i == 0){
-        observe_p[i][0] = 0.81;
+        observes[i][0] = observe_p * observe_p;
         for(int j = 1; j < state_n - 1; j++)
-          observe_p[i][j] = 0.09;
-        observe_p[i][state_n - 1] = 0.01;
+          observes[i][j] = (1 - observe_p) * observe_p;
+        observes[i][state_n - 1] = (1 - observe_p) * (1 - observe_p);
       } else if(i > 0 && i < state_n - 1){
-        observe_p[i][0] = 0.09;
+        observes[i][0] = (1 - observe_p) * observe_p;
         for(int j = 1; j < state_n - 1; j++)
-          observe_p[i][j] = 0.81;
-        observe_p[i][state_n - 1] = 0.09;
+          observes[i][j] = observe_p * observe_p;
+        observes[i][state_n - 1] = observe_p * (1 - observe_p);
       } else if(i == state_n - 1){
-        observe_p[i][0] = 0.01;
+        observes[i][0] = (1 - observe_p) * (1 - observe_p);
         for(int j = 1; j < state_n - 1; j++)
-          observe_p[i][j] = 0.09;
-        observe_p[i][state_n - 1] = 0.81;
+          observes[i][j] = observe_p * (1 - observe_p);
+        observes[i][state_n - 1] = observe_p * observe_p;
       } else{
         System.out.println("state is between 1 and " + state_n + ", but got " + i);
         System.exit(1);
       }
     }
   }
-  public int run(int initial_state, int tMax){
+  public int run(int initial_state, int tMax, boolean monitor){
+    double num = particle_n / state_n;
+    for(int i = 0; i < state_n; i++)
+      particles[i] = num;
     int s = initial_state;
     int o = observe(s);
+    if(monitor) print_situation(s, 0);
     for(int t = 1; t <= tMax; t++){
       int a = choose_action(o);
       int move = act(s, a);
       s += move;
       o = observe(s);
       sampling(a);
-      double[] w = importance(s);
-      resampling(w);
+      importance(s);
+      resampling();
+      if(monitor && t % 10 == 0) print_situation(s, t);
     }
     return s;
   }
@@ -75,36 +84,35 @@ public class ParticleFilter{
       }
     return max_index + 1;
   }
-      
   public int observe(int s){
     double left_rand = Math.random();
     double right_rand = Math.random();
     if(s == 1){
-      if(left_rand < 0.9){
-        if(right_rand < 0.9) return 3;
+      if(left_rand < observe_p){
+        if(right_rand < observe_p) return 3;
         else return 4;
       } else{
-        if(right_rand < 0.9) return 1;
+        if(right_rand < observe_p) return 1;
         else return 2;
       }
-    } else if(s > 1 && s < particles.length){
-      if(left_rand < 0.9){
-        if(right_rand < 0.9) return 1;
+    } else if(s > 1 && s < state_n){
+      if(left_rand < observe_p){
+        if(right_rand < observe_p) return 1;
         else return 2;
       } else{
-        if(right_rand < 0.9) return 3;
+        if(right_rand < observe_p) return 3;
         else return 4;
       }
-    } else if(s == particles.length){
-      if(left_rand < 0.9){
-        if(right_rand < 0.9) return 2;
+    } else if(s == state_n){
+      if(left_rand < observe_p){
+        if(right_rand < observe_p) return 2;
         else return 1;
       } else{
-        if(right_rand < 0.9) return 4;
+        if(right_rand < observe_p) return 4;
         else return 3;
       }
     } else{
-      System.out.println("s is between 1 and " + particles.length + ", but got" + s);
+      System.out.println("s is between 1 and " + state_n + ", but got" + s);
       System.exit(1);
       return ERROR;
     }
@@ -131,14 +139,14 @@ public class ParticleFilter{
       if(s == 1) return STAY;
       else{
         double rand = Math.random();
-        if(rand < 0.8) return LEFT;
+        if(rand < action_p) return LEFT;
         else return STAY;
       }
     } else if(a == RIGHT){
-      if(s == 5) return STAY;
+      if(s == state_n) return STAY;
       else{
         double rand = Math.random();
-        if(rand < 0.8) return RIGHT;
+        if(rand < action_p) return RIGHT;
         else return STAY;
       }
     } else if(a == STAY){
@@ -150,21 +158,21 @@ public class ParticleFilter{
     }
   }
   public void sampling(int a){
-    double[] copy = new double[particles.length];
+    double[] copy = new double[state_n];
     for(int i = 0; i < copy.length; i++)
       copy[i] = 0.0;
     if(a == LEFT){
       copy[0] += particles[0];
       for(int i = 1; i < copy.length; i++){
-        copy[i - 1] += particles[i] * 0.8;
-        copy[i] += particles[i] * 0.2;
+        copy[i - 1] += particles[i] * action_p;
+        copy[i] += particles[i] * (1 - action_p);
       }
     } else if(a == RIGHT){
       for(int i = 0; i < copy.length - 1; i++){
-        copy[i + 1] += particles[i] * 0.8;
-        copy[i] += particles[i] * 0.2;
+        copy[i + 1] += particles[i] * action_p;
+        copy[i] += particles[i] * (1 - action_p);
       }
-      copy[copy.length - 1] += particles[particles.length - 1];
+      copy[copy.length - 1] += particles[state_n - 1];
     } else if(a == STAY){
       copy = particles;
     } else{
@@ -173,25 +181,23 @@ public class ParticleFilter{
     }
     particles = copy;
   }
-  public double[] importance(int s){
-    double[] w = new double[particles.length];
-    for(int i = 0; i < w.length; i++)
-      w[i] = particles[i] * observe_p[s - 1][i];
-    return w;
+  public void importance(int s){
+    for(int i = 0; i < state_n; i++)
+      particles[i] *= observes[s - 1][i];
   }
-  public void resampling(double[] w){
+  public void resampling(){
     double sum = 0.0;
-    for(int i = 0; i < w.length; i++)
-      sum += w[i];
-    for(int i = 0; i < particles.length; i++)
-      particles[i] = particle_n * w[i] / sum;
+    for(int i = 0; i < state_n; i++)
+      sum += particles[i];
+    for(int i = 0; i < state_n; i++)
+      particles[i] *= particle_n / sum;
   }
   public void print_situation(int s, int t){
     System.out.println("t = " + t);
-    System.out.println("Wheel Duck: s = " + s);
+    System.out.println("State: s = " + s);
     System.out.print("Particles: [");
-    for(int i = 0; i < particles.length - 1; i++)
+    for(int i = 0; i < state_n - 1; i++)
       System.out.printf("%.3f , ", particles[i]);
-    System.out.printf("%.3f]\n", particles[particles.length - 1]);
+    System.out.printf("%.3f]\n", particles[state_n - 1]);
   }
 }
--- particle_filter/Test.java
+++ particle_filter/Test.java
@@ -1,12 +1,13 @@
 public class Test{
+  private static final int TESTNUM = 1;
   public static void main(String[] args){
-    ParticleFilter p = new ParticleFilter(5, 10);
+    ParticleFilter p = new ParticleFilter(5, 10, 0.9, 0.8);
     int cnt = 0;
-    for(int i = 0; i < 1000; i++){
+    for(int i = 0; i < TESTNUM; i++){
       p.init();
-      int result = p.run(3, 100);
+      int result = p.run(3, 100, true);
       if(p.predict() == result) cnt++;
     }
-    System.out.println("Predict Correctness = " + (cnt / 1000.0));
+    System.out.println("Predict Accuracy = " + ((double)cnt / TESTNUM));
   }
 }
